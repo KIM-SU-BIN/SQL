2beb8c2c5599555a215201e7f8329cea54cfb265
subintoto <subintoto@naver.com>
2022-05-18 15:47:53 +0900

    0518 ROWNUM

--- Ex/Ex04.sql
+++ Ex/Ex04.sql
@@ -189,4 +189,191 @@ FROM employees e ,(SELECT MAX(salary) maxSalary,
                   FROM employees
                   group by department_id)s
 where e.department_id = s.department_id             -->> 중복값 제거
-and e.salary = s.maxSalary;                            
+and e.salary = s.maxSalary;      
+
+----------------------------------------------------------------------------
+/*<5월 18일 오전> PPT04
+[예제] 급여를 가장 많이 받는 5명의 직원의 이름을 출력하시오.
+1. 급여순으로 정렬 */
+SELECT  employee_id, 
+        first_name, 
+        salary
+FROM employees
+ORDER BY salary desc;
+
+--2.1~5등만 출력 => order by 사용하면 ﻿rownum 정렬 흐뜨러짐(rownum과 order by 같이 사용불가)
+SELECT  ROWNUM,
+        employee_id, 
+        first_name, 
+        salary
+FROM employees
+where ROWNUM >= 1
+and ROWNUM <=5 ;
+
+--최종
+SELECT  ROWNUM,
+        ot.employee_id,
+        ot.first_name,
+        ot.salary               --컬럼 추가하려면 from에 있는 selec에 추가된 컬럼이 있어야 사용할 수 있음!
+FROM (SELECT  employee_id, 
+              first_name, 
+              salary
+      FROM employees
+      ORDER BY salary desc) ot 
+where ROWNUM >= 1
+and ROWNUM <= 5;
+
+-- 3 ~ 5등까지 : 데이터가 없어서 구할 수 없는 것 / 현재 식은 시작이 1이 아니면 구할 수 없음!
+SELECT  ROWNUM rn,
+        ot.employee_id,
+        ot.first_name,
+        ot.salary               --컬럼 추가하려면 from에 있는 selec에 추가된 컬럼이 있어야 사용할 수 있음!
+FROM (SELECT  employee_id, 
+              first_name, 
+              salary
+      FROM employees
+      ORDER BY salary desc) ot 
+where ROWNUM >= 3
+and ROWNUM <= 5;
+
+--3 ~ 5 최종
+SELECT *
+FROM (SELECT  ROWNUM rn,
+        ot.employee_id,
+        ot.first_name,
+        ot.salary               --컬럼 추가하려면 from에 있는 selec에 추가된 컬럼이 있어야 사용할 수 있음!
+      FROM (SELECT  employee_id, 
+                    first_name, 
+                    salary
+            FROM employees
+            ORDER BY salary desc) ot 
+            ) ort
+where rn >= 2
+and rn< 5;
+
+--최종
+-->(1)정렬하고 (2)rownum이 있는 테이블을 사용하고 (3)where절을 쓴다
+select  ort.rn,
+        ort.first_name,
+        ort.salary
+from (select  rownum rn,
+              ot.employee_id,
+              ot.first_name,
+              ot.salary
+      from (select  employee_id,
+                    first_name,
+                    salary
+            from employees
+            order by salary desc) ot   --(1)
+      )ort  --(2)
+where rn >= 2
+and rn<=5;  --(3)
+
+----------------------------------------------------------------------------
+/*<5월 18일 오전> PPT04
+[예제] 급여를 가장 많이 받는 5명의 직원의 이름을 출력하시오.
+1. 급여순으로 정렬 */
+SELECT  employee_id, 
+        first_name, 
+        salary
+FROM employees
+ORDER BY salary desc;
+
+--2.1~5등만 출력 => order by 사용하면 ﻿rownum 정렬 흐뜨러짐(rownum과 order by 같이 사용불가)
+SELECT  ROWNUM,
+        employee_id, 
+        first_name, 
+        salary
+FROM employees
+where ROWNUM >= 1
+and ROWNUM <=5 ;
+
+--최종
+SELECT  ROWNUM,
+        ot.employee_id,
+        ot.first_name,
+        ot.salary               --컬럼 추가하려면 from에 있는 selec에 추가된 컬럼이 있어야 사용할 수 있음!
+FROM (SELECT  employee_id, 
+              first_name, 
+              salary
+      FROM employees
+      ORDER BY salary desc) ot 
+where ROWNUM >= 1
+and ROWNUM <= 5;
+
+-- 3 ~ 5등까지 : 데이터가 없어서 구할 수 없는 것 / 현재 식은 시작이 1이 아니면 구할 수 없음!
+SELECT  ROWNUM rn,
+        ot.employee_id,
+        ot.first_name,
+        ot.salary               --컬럼 추가하려면 from에 있는 selec에 추가된 컬럼이 있어야 사용할 수 있음!
+FROM (SELECT  employee_id, 
+              first_name, 
+              salary
+      FROM employees
+      ORDER BY salary desc) ot 
+where ROWNUM >= 3
+and ROWNUM <= 5;
+
+--3 ~ 5 최종
+SELECT *
+FROM (SELECT  ROWNUM rn,
+        ot.employee_id,
+        ot.first_name,
+        ot.salary               --컬럼 추가하려면 from에 있는 selec에 추가된 컬럼이 있어야 사용할 수 있음!
+      FROM (SELECT  employee_id, 
+                    first_name, 
+                    salary
+            FROM employees
+            ORDER BY salary desc) ot 
+            ) ort
+where rn >= 2
+and rn< 5;
+
+--최종
+-->(1)정렬하고 (2)rownum이 있는 테이블을 사용하고 (3)where절을 쓴다
+select  ort.rn,
+        ort.first_name,
+        ort.salary
+from (select  rownum rn,
+              ot.employee_id,
+              ot.first_name,
+              ot.salary
+      from (select  employee_id,
+                    first_name,
+                    salary
+            from employees
+            order by salary desc) ot   --(1)
+      )ort  --(2)
+where rn >= 2
+and rn<=5;  --(3)
+
+------------------------------------------------------------------------
+
+--[예제] 07년에 입사한 직원중 급여가 많은 직원중 3에서 7등의 이름 급여 입사일은? 
+--07년 입사자
+SELECT  first_name,
+        hire_date,
+        salary
+FROM employees
+WHERE hire_date >= '07/01/01'
+and hire_date <= '07/12/31'
+ORDER BY salary desc;
+
+--최종
+SELECT  ro.rn,
+        ro.first_name,
+        ro.salary
+from (SELECT ROWNUM rn,
+            ye.first_name,
+            ye.hire_date,
+            ye.salary
+        FROM (SELECT  first_name,
+                      hire_date,
+                      salary
+                FROM employees
+                WHERE hire_date >= '07/01/01'       --'07%'로 사용 가능하나 오류가 있을 수는 있음
+                and hire_date <= '07/12/31'
+                ORDER BY salary desc) ye
+        )ro
+where rn >= 3
+and rn <= 7;
